4c5649daaf5707ff2e32ab1bbbcc3dcc2bc0a5b0
DINA <dinaalaagalal@gmail.com>
2020-03-19 15:30:48 +0200

    version2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{FormsModule} from"@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatNativeDateModule } from "@angular/material/core";
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 
@@ -92,11 +92,12 @@ import { NetworkComponent } from './network/network.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
 
     FlexLayoutModule,
-
+    MatNativeDateModule,
     MatDividerModule,
     MatCheckboxModule,
     MatCheckboxModule,
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -13,8 +13,8 @@ import { RequestRecommendationComponent } from '../dialogues/request-recommendat
 import { SummaryComponent } from '../dialogues/summary/summary.component';
 import { WorkExperienceComponent } from '../dialogues/work-experience/work-experience.component';
 import { LinksComponent } from '../dialogues/links/links.component';
-
-
+import{WorkExperiencesServeicesService}from'../dialogues/work-experience/work-experiences-serveices.service';
+import { Experience } from "../Model/Experience";
 
 @Component({
   selector: "app-profile",
@@ -22,8 +22,41 @@ import { LinksComponent } from '../dialogues/links/links.component';
   styleUrls: ["./profile.component.scss"]
 })
 export class ProfileComponent implements OnInit {
-  ngOnInit(): void { }
-  constructor(private dialog: MatDialog) { }
+
+  workplaces: Experience[];
+  myexp: Experience;
+  ngOnInit(): void {
+
+    this.workplaces = this.workserv.getall();
+      
+   }
+  constructor(private dialog: MatDialog, public workserv: WorkExperiencesServeicesService) { }
+
+
+editDialog(e: any, workex: Experience){
+  let arr = [LanguageComponent, SkillsComponent, EducationComponent,
+    RequestRecommendationComponent, SummaryComponent, WorkExperienceComponent, LinksComponent]
+  let temp: any = e + 'Component';
+  // console.log(temp);
+  let res = arr.filter(comp => comp.name == temp)
+  this.myexp=this.workserv.getbyId(workex.id);
+  console.log(this.myexp)
+  console.log( res)
+  const dialogRef = this.dialog.open(res[0], {
+    width: "550px",
+    data: {
+    empdata: this.myexp
+    // empdata:'hamada'
+    }
+  });
+  dialogRef.afterClosed().subscribe(result=>{
+
+    console.log(`here is the data result ${result}`);
+  })
+ 
+  console.log(workex);
+
+}
 
   openDialog(e: any): void {
     let arr = [LanguageComponent, SkillsComponent, EducationComponent,
